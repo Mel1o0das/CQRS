64d2c7eda9ebec669dc9706f4b6fae8d0044eb0f
Mel1o0das <m4rsik2016@yande.ru>
2025-02-10 20:21:41 +0300

    register our service

---
+++ Application/DependecyInjection.cs
@@ -0,0 +1,19 @@
+using Application.Topics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplictionServices(
+                this IServiceCollection services,
+                IConfiguration configuration
+            )
+        {
+            services.AddScoped<ITopicService, TopicService>();
+
+            return services;
+        }
+    }
+}
