50929b7ab3a3ccf4a157649599d98b7ca2e384ed
mel1o0das <m4rsik2016@yandex.ru>
2025-02-17 20:35:40 +0300

    add fields for soft delete

--- Domain/Abstractions/Entity.cs
+++ Domain/Abstractions/Entity.cs
@@ -3,4 +3,7 @@ namespace Domain.Abstractions;
 public abstract class Entity<T> : IEntity<T>
 {
     public required T Id { get; set; }
+
+    public bool IsDeleted { get; set; } = default!;
+    public DateTimeOffset? DeletedAt { get; set; } = default!;
 }
