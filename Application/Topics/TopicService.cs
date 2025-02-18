6c82cafcd1ccb111d6aa03698cbab2dbee4dfbe4
mel1o0das <m4rsik2016@yandex.ru>
2025-02-18 18:47:54 +0300

    realize soft delete

--- Application/Topics/TopicService.cs
+++ Application/Topics/TopicService.cs
@@ -33,34 +33,38 @@ public class TopicService
     public async Task DeleteTopicAsync(Guid id)
     {
         TopicId topicId = TopicId.Of(id);
-        var result = await dbContext.Topics.FindAsync([topicId]);
+        var topic = await dbContext.Topics.FindAsync([topicId]);
 
-        if (result is null)
+        if (topic is null || topic.IsDeleted)
         {
             throw new TopicNotFoundException(id);
         }
 
-        dbContext.Topics.Remove(result);
+        topic.IsDeleted = true;
+        topic.DeletedAt = DateTimeOffset.UtcNow;
+
+        //dbContext.Topics.Remove(topic);
         await dbContext.SaveChangesAsync(CancellationToken.None);
     }
 
     public async Task<TopicResponseDto> GetTopicAsync(Guid id)
     {
         TopicId topicId = TopicId.Of(id);
-        var result = await dbContext.Topics.FindAsync([topicId]);
+        var topic = await dbContext.Topics.FindAsync([topicId]);
 
-        if (result is null)
+        if (topic is null || topic.IsDeleted)
         {
             throw new TopicNotFoundException(id);
         }
 
-        return result.ToTopicResponseDto();
+        return topic.ToTopicResponseDto();
     }
 
     public async Task<List<TopicResponseDto>> GetTopicsAsync()
     {
         var topics = await dbContext.Topics
             .AsNoTracking()
+            .Where(t => !t.IsDeleted)
             .ToListAsync();
 
         return topics.ToTopicResponseDtoList();
@@ -72,7 +76,7 @@ public class TopicService
 
         var topic = await dbContext.Topics.FindAsync([topicId]);
 
-        if (topic is null)
+        if (topic is null || topic.IsDeleted)
         {
             throw new TopicNotFoundException(id);
         }
