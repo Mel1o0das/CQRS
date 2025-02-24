49888c4bb9042a3a74d0944afd5a2056403094c4
mel1o0das <m4rsik2016@yandex.ru>
2025-02-24 20:40:46 +0300

    initial identity user

---
+++ Domain/Security/CustomIdentityUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Security;
+
+public class CustomIdentityUser : IdentityUser
+{
+    public string FullName { get; set; } = default!;
+    public string About { get; set; } = default!;
+}
