80d0108cf828b43b889f1699e0395e1e6893d44d
mel1o0das <m4rsik2016@yandex.ru>
2025-03-17 20:19:40 +0300

    realize login controller

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,10 +1,37 @@
+using Domain.Security;
+using Domain.Security.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase
+public class AuthController(UserManager<CustomIdentityUser> manager)
+    : ControllerBase
 {
+    [HttpPost("login")]
+    public async Task<IResult> Login(LoginRequest dto)
+    {
+        var user = await manager.FindByEmailAsync(dto.Email);
 
+        if (user is null)
+        {
+            return Results.Unauthorized();
+        }
+
+        var result = await manager.CheckPasswordAsync(user, dto.Password);
+
+        if (result)
+        {
+            var response = new IdentityUserResponceDto(
+                user.UserName!, user.Email!, "jwt"
+            );
+
+            return Results.Ok(new { result = response });
+        }
+
+        return Results.Unauthorized();
+    }
 }
---
+++ Domain/Security/Dtos/LoginRequestDto.cs
@@ -0,0 +1,6 @@
+namespace Domain.Security.Dtos;
+
+public record LoginRequestDto(
+    string Email,
+    string Password
+);
